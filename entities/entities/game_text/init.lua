6fbddd602c96d6d036c51669cde5b67b9763c28e
Forrest Keller <forrestmarkx@outlook.com>
2018-03-28 00:39:04 -0500

    Use Color1 keyvalue for the message in game_text
    Added support for the other game_text keyvalues but they are currently unused

--- entities/entities/game_text/init.lua
+++ entities/entities/game_text/init.lua
@@ -18,6 +18,10 @@ function ENT:KeyValue(key, value)
 		self.FadeOutTime = self.FadeOutTime or tonumber(value)
 	elseif key == "holdtime" then
 		self.HoldTime = self.HoldTime or tonumber(value)
+	elseif key == "effect" then
+		self.EffectType = self.EffectType or tonumber(value)	
+	elseif key == "fxtime" then
+		self.FXTime = self.FXTime or tonumber(value)
 	elseif key == "message" then
 		self.Message = self.Message or value
 	elseif string.Left(key, 2) == "on" then
@@ -39,21 +43,20 @@ function ENT:InputDisplay(activator)
 	self:Display(activator)
 end
 
+-- ToDo: Add support for m_textParms.effect from C++ game_text
 function ENT:Display(activator)
 	if not self:CanFireForActivator(activator) then
 		return
 	end
 
 	local messagetab = {
-		Color1 = self.Color1,
-		Color2 = self.Color2,
 		XPos = self.XPos,
 		YPos = self.YPos,
 		FadeInTime = self.FadeInTime,
 		FadeOutTime = self.FadeOutTime,
 		HoldTime = self.HoldTime,
 		Font = "zm_game_text_small",
-		Message = {self.Message}
+		Message = {self.Color1, self.Message}
 	}
 	if self:MessageToAll() then
 		util.PrintMessageBold("GameText_"..self:EntIndex(), messagetab)
